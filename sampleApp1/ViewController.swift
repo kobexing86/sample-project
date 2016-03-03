74fb4d953f9232c74afc372ac361687fdae13036
kobexing86 <kobexing86@gmail.com>
2016-03-03 15:55:34 -0500

    check in sample app

--- sampleApp1/ViewController.swift
+++ sampleApp1/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redImg: UIImageView!
 
+    @IBOutlet weak var btnRed: UIButton!
+    @IBOutlet weak var btnBlue: UIButton!
+    @IBOutlet weak var blueImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onClickHideBlue(sender: AnyObject) {
+        blueImg.hidden = true
+    }
 
+    @IBAction func OnClickHideRed(sender: AnyObject) {
+        redImg.hidden = true
+    }
 }
 
